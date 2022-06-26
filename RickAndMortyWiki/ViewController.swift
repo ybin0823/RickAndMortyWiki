755afd52246cfa4ff21a1428b586ba7a25468e01
jyb <jyb0823@gmail.com>
2022-06-26 16:18:05 +0900

    Initial Commit

---
+++ RickAndMortyWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyWiki
+//
+//  Created by jyb on 2022/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
