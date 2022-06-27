1792a963c9f38c3933b7119027ee9d1cb5645c63
jyb <jyb0823@gmail.com>
2022-06-27 13:34:03 +0900

    기본 탭 구조 추가(캐릭터, 로케이션, 검색)

---
+++ RickAndMortyWiki/Presentation/View/MainTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabBarController.swift
+//  RickAndMortyWiki
+//
+//  Created by jyb on 2022/06/27.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
