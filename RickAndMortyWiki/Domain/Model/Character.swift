997882dc7001d803e0f9508a152929c261925d2d
jyb <jyb0823@gmail.com>
2022-06-27 13:52:15 +0900

    character json test

---
+++ RickAndMortyWiki/Domain/Model/Character.swift
@@ -0,0 +1,43 @@
+//
+//  Character.swift
+//  RickAndMortyWiki
+//
+//  Created by jyb on 2022/06/27.
+//
+
+import Foundation
+
+enum CharacterStatus {
+    case alive
+    case dead
+    case unknown
+}
+
+enum Gender {
+    case male
+    case female
+    case genderless
+    case unknown
+}
+
+struct Character: Codable {
+    let id: Int
+    let name: String
+//    let status: CharacterStatus
+    let species: String
+    let type: String
+//    let gender: Gender
+    let url: String
+    let created: String
+//    let episode: [String]
+}
+
+struct CharcaterOrigin {
+    let name: String
+    let url: String
+}
+
+struct CharacterLocation {
+    let name: String
+    let url: String
+}
