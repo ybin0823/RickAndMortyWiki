acd74121eb4c0160df43151e4a8eee237e814a42
jyb <jyb0823@gmail.com>
2022-06-28 14:37:31 +0900

    검색시 값이 없을 경우 empty 처리 되도록 보완

--- RickAndMortyWiki/Presentation/ViewModel/SearchViewModel.swift
+++ RickAndMortyWiki/Presentation/ViewModel/SearchViewModel.swift
@@ -31,20 +31,29 @@ class SearchViewModel {
         placeHolderText.onNext("\(type.rawValue) Search")
     }
     
+    //TODO: - search 시 404 에러는 어떻게 처리하면 좋을지..?
+    // ApiService에서 공통으로 처리하면 empty가 아닌 url path를 잘못 입력한 경우를 알기가 어려움
+    // Info를 nullable로 처리하면 응답이 마치 nullable일 듯한 의미가 되기 때문에 적합하지 않아보임
     func search(text: String) {
         switch type {
         case .character:
-            repository.getCharacters(name: text).subscribe(onNext: {
+            repository.getCharacters(name: text)
+                .catchAndReturn(Characters(info: Info(count: 0, pages: 0, next: nil, prev: nil), results: []))
+                .subscribe(onNext: {
                 self.results.onNext($0.results)
                 self.info = $0.info
             }).disposed(by: disposeBag)
         case .location:
-            repository.getLocations(name: text).subscribe(onNext: {
+            repository.getLocations(name: text)
+                .catchAndReturn(Locations(info: Info(count: 0, pages: 0, next: nil, prev: nil), results: []))
+                .subscribe(onNext: {
                 self.results.onNext($0.results)
                 self.info = $0.info
             }).disposed(by: disposeBag)
         case .episode:
-            repository.getEpisodes(name: text).subscribe(onNext: {
+            repository.getEpisodes(name: text)
+                .catchAndReturn(Episodes(info: Info(count: 0, pages: 0, next: nil, prev: nil), results: []))
+                .subscribe(onNext: {
                 self.results.onNext($0.results)
                 self.info = $0.info
             }).disposed(by: disposeBag)
