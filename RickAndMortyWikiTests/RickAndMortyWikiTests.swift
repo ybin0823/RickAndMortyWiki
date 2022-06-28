c075fc34433a38424a05513df9d804b71e8f3571
jyb <jyb0823@gmail.com>
2022-06-28 15:41:17 +0900

    viewModel 참고할 test코드 추가

--- RickAndMortyWikiTests/RickAndMortyWikiTests.swift
+++ RickAndMortyWikiTests/RickAndMortyWikiTests.swift
@@ -6,8 +6,28 @@
 //
 
 import XCTest
+import RxSwift
+import RxBlocking
 @testable import RickAndMortyWiki
 
+
+class MockCharacterRepositoy: CharacterRepository {
+    func getCharacters() -> Observable<Characters> {
+        return Observable.just(Characters(info: Info(count: 0, pages: 0, next: nil, prev: nil), results: []))
+    }
+    
+    func getNextCharacters(url: String) -> Observable<Characters> {
+        return Observable.just(Characters(info: Info(count: 0, pages: 0, next: nil, prev: nil), results: []))
+    }
+    
+    func getCharacter(id: Int) -> Observable<Character> {
+        return Observable.just(Character(id: 0, name: "", status: .unknown, species: "", type: "", gender: .unknown, url: "", created: "", image: ""))
+    }
+    
+    
+}
+
+//TODO: - created를 ui맞춰서 formatting을 한다면 사용하기 좋을 것 같음
 class RickAndMortyWikiTests: XCTestCase {
 
     override func setUpWithError() throws {
@@ -24,6 +44,14 @@ class RickAndMortyWikiTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        
+        let viewModel = CharactersViewModel(repository: MockCharacterRepositoy())
+        do {
+            let result = try viewModel.characters.toBlocking(timeout: nil).first()
+            XCTAssertTrue(result!.isEmpty)
+        } catch {
+            
+        }
     }
 
     func testPerformanceExample() throws {
